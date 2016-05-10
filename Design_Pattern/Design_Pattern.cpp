e2ad745df18a77b3f3bf99436dea5384412cd6be
Lee Jin-Woo <came1230@naver.com>
2016-05-11 05:44:03 +0900

    디자인패턴: 전략(Strategy) 패턴 구현
    - 플레이어로 행동과 공격을 전략패턴으로 구현

---
+++ Design_Pattern/Design_Pattern.cpp
@@ -0,0 +1,15 @@
+// Design_Pattern.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "Strategy.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	StrategyClient strategyClient;
+
+	strategyClient.Play();
+
+	return 0;
+}
+
