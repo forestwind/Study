44f4f347ea9a840e5e712ba6bec9da92c2867983
Lee Jin-Woo <came1230@naver.com>
2016-05-12 02:26:37 +0900

    디자인패턴:단일체(Singleton) 패턴 구현
    게임 매니저 구현

--- Design_Pattern/Design_Pattern.cpp
+++ Design_Pattern/Design_Pattern.cpp
@@ -3,12 +3,19 @@
 
 #include "stdafx.h"
 #include "Strategy.h"
+#include "Singleton.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	StrategyClient strategyClient;
 
 	strategyClient.Play();
+
+	cout << endl;
+
+	SingletonClient singletonClient;
+
+	singletonClient.play();
 
 	return 0;
 }
